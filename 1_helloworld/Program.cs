82fd94c43ed9c77dc087f2fb382af7d26fc98ab4
Dung-CoDienTu <maiquocdung95@gmail.com>
2022-03-27 10:57:50 +0900

    Add pro HelloWorld

---
+++ 1_helloworld/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_helloworld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world");
+            Console.WriteLine("Ban ten gi?");
+            string Ten = "";
+            Ten = Console.ReadLine();
+            Console.WriteLine("Ban may tuoi?");
+            int Tuoi = 0;
+            Tuoi = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ban {0} nam nay {1} tuoi.", Ten, Tuoi);
+            Console.Read();
+        }
+    }
+}
